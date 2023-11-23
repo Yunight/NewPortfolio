c9f10e5a7eb07cbdd86d94d98a64a182e6c7dcfc
Benchi <benchi.chen.bc@gmail.com>
2023-11-23 15:18:46 +0100

    mes projets part

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+import Project from "./project";
 
 function Projects() {
   return (
     <section>
       <SectionHeading>mes projects</SectionHeading>
-      <div></div>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
     </section>
   );
 }
--- lib/data.ts
+++ lib/data.ts
@@ -72,7 +72,7 @@ export const projectsData = [
     title: "CorpComment",
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+    tags: ["React", "Next.js", "Tailwind"],
     imageUrl: corpcommentImg,
   },
   {
