c49358065e8e1bc3d571a860e13e13ec8f10d9cd
Benchi <benchi.chen.bc@gmail.com>
2023-11-25 02:01:18 +0100

    theme button ajustment

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -9,7 +9,7 @@ function ThemeSwitch() {
 
   return (
     <button
-      className="fixed top-6 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
+      className="fixed bottom-6 right-5 md:top-6 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
       onClick={toggleTheme}
     >
       {theme === "light" ? <BsSun /> : <BsMoon />}
