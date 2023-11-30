4da554a98ae31b3b158cb986246b8447443116f3
Benchi <benchi.chen.bc@gmail.com>
2023-11-30 22:14:24 +0100

    adding border to white theme

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -9,7 +9,7 @@ function ThemeSwitch() {
 
   return (
     <button
-      className="fixed bottom-6 right-5 flex h-[3rem] w-[3rem] items-center justify-center rounded-full border border-white border-opacity-40 bg-white bg-opacity-80 shadow-2xl backdrop-blur-[0.5rem] transition-all hover:scale-[1.15] active:scale-105 dark:bg-gray-950 md:top-6"
+      className="fixed bottom-6 right-5 flex h-[3rem] w-[3rem] items-center justify-center rounded-full border border-gray-500 border-opacity-40 bg-white bg-opacity-80 shadow-2xl backdrop-blur-[0.5rem] transition-all hover:scale-[1.15] active:scale-105 dark:border-white dark:bg-gray-950 md:top-6"
       onClick={toggleTheme}
     >
       {theme === "light" ? <BsSun /> : <BsMoon />}
